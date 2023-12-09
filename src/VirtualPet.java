f1c73f10dbd836fb926601260809c7436ce69c93
rdmiller00 <rd.miller0619@gmail.com>
2023-12-09 08:53:25 -0500

    Initial game requirements complete

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,3 +1,151 @@
+import java.util.Random;
+
 public class VirtualPet{
+    //attributes
+    private String name;
+    private int hungerLevel;
+    private int boredomLevel;
+    private int thirstLevel;
+    private int affectionLevel;
+    
+    public VirtualPet(String name) {
+        this.name = name;
+        this.hungerLevel = new Random().nextInt(100) + 1;
+        this.boredomLevel = new Random().nextInt(100) + 1;
+        this.thirstLevel = new Random().nextInt(100) + 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHungerLevel() {
+        return hungerLevel;
+    }
+
+    public void setHungerLevel(int hungerLevel) {
+        this.hungerLevel = hungerLevel;
+    }
+
+    public int getBoredomLevel() {
+        return boredomLevel;
+    }
+
+    public void setBoredomLevel(int boredomLevel) {
+        this.boredomLevel = boredomLevel;
+    }
+
+    public int getThirstLevel() {
+        return thirstLevel;
+    }
+
+    public void setThirstLevel(int thirstLevel) {
+        this.thirstLevel = thirstLevel;
+    }
+
+    public int getAffectionLevel() {
+        return affectionLevel;
+    }
+
+    public void setAffectionLevel(int affectionLevel) {
+        this.affectionLevel = affectionLevel;
+    }
+
+
+    public void hungerCheck(){
+        if (hungerLevel > 75) {
+            System.out.print(" They are getting pretty hungry. In fact his hunger level is " + hungerLevel +". ");
+        } else {
+            System.out.print(" Their hunger level is " + hungerLevel + ". ");
+        }
+    }
+
+    public void thirstCheck(){
+        if (thirstLevel > 75) {
+            System.out.print(name + " is getting pretty thirsty! I would get them some water ASAP before you have to make a trip to the vet.");
+        } else {
+            System.out.print(name + " seems to be doing great, make sure you keep giving them plenty of water.");
+        }
+    }
+
+    public void boredomCheck(){
+        if (boredomLevel > 90) {
+            System.out.println(name + " just chewed up the couch. You need to get him some more activity!");
+        } else if (boredomLevel > 75) {
+            System.out.print(name + " is quite bored...I would play fetch or go for a walk before they start destroying things.");
+        } else {
+            boredomLevel += 5;
+            System.out.print(name + " seems to have had plenty of activity today, keep up the good work.");
+        }
+    }
+
+    public void affectionCheck(){
+        if (affectionLevel > 60) {
+            System.out.println(name + " is feeling so loved today, just look at that tail wagging!");
+        } else {
+            System.out.println(name + " might need some extra lovin...I know things have been busy, but they miss you.");
+        }
+    }
+    
+    public void petCheck(){
+        thirstCheck();
+        hungerCheck();
+        boredomCheck();
+        affectionCheck();
+    }
+
+    public void tick(){
+        boredomLevel += 5;
+        getFood();
+        getWater();
+        affectionLevel -= 5;
+    }
+
+    public void playFetch(){
+        boredomLevel -= 15;
+        hungerLevel += 10;
+        thirstLevel += 25;
+        affectionLevel += 15;
+        System.out.println("What a great game of fetch!");
+        tick();
+    }
+
+    public void goForWalk(){
+        boredomLevel -= 10;
+        hungerLevel += 5;
+        thirstLevel += 5;
+        affectionLevel += 10;
+        System.out.println("What a good walk. That quality time means so much to " + name + ".");
+        tick();
+    }
+
+    public void snuggle(){
+        boredomLevel -= 10;
+        affectionLevel += 30;
+        System.out.println("THAT IS THE GOOD STUFF! " + name + " goes crazy for those snuggles.");
+        tick();
+    }
+
+    public void getWater(){
+        if (thirstLevel > 55) {  
+        thirstLevel -= 20;
+        } hungerLevel += 5;
+    }
+
+    public void getFood(){
+        if(hungerLevel > 75){
+        hungerLevel -= 30;
+        } hungerLevel += 5;
+    }
+
+    public void giveFoodAndWater(){
+        thirstLevel = 5;
+        hungerLevel = 5;
+    }
+
 
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,5 +1,60 @@
+import java.util.Scanner;
+
 public class VirtualPetApp {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.print("What is your pet's name? ");
+        String name = scanner.nextLine();
+
+        VirtualPet pet1 = new VirtualPet(name);
+
+        System.out.println("That is really cool having a pet is a lot of responsibility, but honestly if you just spend time with them and care for them it is easy. Here is how " + name + " is doing right now:");
+        pet1.petCheck();
+
+        int userChoice;
+    do{
+        System.out.println("What would you like to do today?");
+        System.out.println("Enter 1 to check on your pet");
+        System.out.println("Enter 2 to give " + name + " some food and water");
+        System.out.println("Enter 3 to play fetch");
+        System.out.println("Enter 4 to go for a walk");
+        System.out.println("Enter 5 to snuggle");
+        System.out.println("Enter 6 to exit the game");
+
+        userChoice = scanner.nextInt();
+        
+
+        switch (userChoice) {
+            case 1:
+                pet1.petCheck();
+                break;
+            case 2:
+                pet1.giveFoodAndWater();;
+                break;
+            case 3:
+                pet1.playFetch();
+                break;
+            case 4:
+                pet1.goForWalk();
+                break;
+            case 5:
+                pet1.snuggle();
+                break;
+            case 6:
+                System.out.println("Thanks for playing, you and " + name + " have a great day!");
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter 1-6!");
+                break;
+
+
+    }   if (pet1.getHungerLevel() > 100) {
+            System.out.println("RIP");
+            System.exit(0);
     }
+    
+    } while (userChoice != 6);
+        scanner.close();
+}
 }
